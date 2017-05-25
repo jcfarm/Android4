eced4b06d186f4f2e988935a8b1ad2b12c9c4eda
zhangnan <409001081@qq.com>
2017-05-25 08:44:45 +0800

    Video界面完成

--- MyFarm/app/src/main/java/com/example/zhangnan/myfarm/MonitorActivity.java
+++ MyFarm/app/src/main/java/com/example/zhangnan/myfarm/MonitorActivity.java
@@ -2,20 +2,20 @@ package com.example.zhangnan.myfarm;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.VideoView;
-import com.example.zhangnan.myfarm.swipecardrecyclerview.ItemRemovedListener;
-import com.example.zhangnan.myfarm.swipecardrecyclerview.SwipeCardAdapter;
-import com.example.zhangnan.myfarm.swipecardrecyclerview.SwipeCardLayoutManager;
-import com.example.zhangnan.myfarm.swipecardrecyclerview.SwipeCardRecyclerView;
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
 
 /**
  * Created by zhangnan on 17/5/11.
@@ -23,77 +23,115 @@ import java.util.List;
 
 public class MonitorActivity extends AppCompatActivity {
 
-    private SwipeCardRecyclerView mRecyclerView;
-    private MyAdapter mAdapter;
-    private int nowIndex;
+    private RecyclerView recyclerView;
+    private RecyclerViewVideoAdapter adapterVideoList;
+    private LinearLayoutManager mLayoutManager;
+    private ImageView mImageView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor);
         getWindow().setStatusBarColor(getResources().getColor(R.color.app_green));
 
-        mRecyclerView = (SwipeCardRecyclerView) findViewById(R.id.swipe_card_recycler_view);
-        mRecyclerView.setLayoutManager(new SwipeCardLayoutManager());
-        final List<String> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            list.add(String.valueOf("http://video.jiecao.fm/11/23/xin/%E5%81%87%E4%BA%BA.mp4"));
-        }
-        mAdapter = new MyAdapter(this, list);
-        mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.setRemovedListener(new ItemRemovedListener() {
+        mImageView = (ImageView) findViewById(R.id.position_video);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_video);
+        recyclerView.setLayoutManager(mLayoutManager = new LinearLayoutManager(this));
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onRightRemoved() {
-                //Toast.makeText(MonitorActivity.this, nowIndex + " was right removed", Toast.LENGTH_SHORT).show();
-                nowIndex--;
-                mAdapter.notifyDataSetChanged();
+            public void onClick(View v) {
+                //定位测试
+                mLayoutManager.scrollToPositionWithOffset(3, 0);
+            }
+        });
+
+        adapterVideoList = new RecyclerViewVideoAdapter(this);
+        recyclerView.setAdapter(adapterVideoList);
+        recyclerView.addOnChildAttachStateChangeListener(new RecyclerView.OnChildAttachStateChangeListener() {
+            @Override
+            public void onChildViewAttachedToWindow(View view) {
+
             }
 
             @Override
-            public void onLeftRemoved() {
-                //Toast.makeText(MonitorActivity.this, nowIndex + " was left removed", Toast.LENGTH_SHORT).show();
-                nowIndex--;
-                mAdapter.notifyDataSetChanged();
+            public void onChildViewDetachedFromWindow(View view) {
+//                if (JCVideoPlayerManager.getCurrentJcvdOnFirtFloor() != null) {
+//                    JCVideoPlayer videoPlayer = (JCVideoPlayer) JCVideoPlayerManager.getCurrentJcvdOnFirtFloor();
+//                    if (((ViewGroup) view).indexOfChild(videoPlayer) != -1 && videoPlayer.currentState == JCVideoPlayer.CURRENT_STATE_PLAYING) {
+//                        JCVideoPlayer.releaseAllVideos();
+//                    }
+//                }
             }
         });
     }
 
+    class RecyclerViewVideoAdapter extends RecyclerView.Adapter<RecyclerViewVideoAdapter.MyViewHolder> {
 
-    public class MyAdapter extends SwipeCardAdapter<MyAdapter.MyHolder> {
-        private Context mContext;
+        int[] videoIndexs = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        private Context context;
+        public static final String TAG = "RecyclerViewVideoAdapter";
 
-        public MyAdapter(Context context, List<String> list) {
-            super(list);
-            mContext = context;
-            nowIndex=mList.size()-1;
-        }
-
-
-        @Override
-        public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(mContext).inflate(R.layout.monitor_list_item, parent, false);
-            return new MyHolder(view);
+        public RecyclerViewVideoAdapter(Context context) {
+            this.context = context;
         }
 
         @Override
-        public void onBindViewHolder(MyHolder holder, int position) {
-                if (position == nowIndex){
-                    holder.setData(Uri.parse((String)mList.get(mList.size()-1)));
-                    holder.mVideoView.start();
-                }
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    context).inflate(R.layout.monitor_list_item, parent,
+                    false));
+            return holder;
         }
 
-        public class MyHolder extends RecyclerView.ViewHolder {
-            public VideoView mVideoView;
-            public MyHolder(View itemView) {
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+
+            holder.jcVideoPlayer.setUp(
+                    VideoConstant.videoUrls[0][position], JCVideoPlayer.SCREEN_LAYOUT_LIST,
+                    VideoConstant.videoTitles[0][position]);
+            Picasso.with(holder.jcVideoPlayer.getContext())
+                    .load(VideoConstant.videoThumbs[0][position])
+                    .into(holder.jcVideoPlayer.thumbImageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return videoIndexs.length;
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            JCVideoPlayerStandard jcVideoPlayer;
+
+            public MyViewHolder(View itemView) {
                 super(itemView);
-                mVideoView = (VideoView)itemView.findViewById(R.id.monitor_video);
-            }
-
-            public void setData(Uri path) {
-                mVideoView.setVideoURI(path);
+                jcVideoPlayer = (JCVideoPlayerStandard) itemView.findViewById(R.id.video_player);
             }
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (JCVideoPlayer.backPress()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        JCVideoPlayer.releaseAllVideos();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public static Intent newIntent(Context packageContext){
