79c47c5d953f8b99ed500679a447e06c1050255c
zhangnan <409001081@qq.com>
2017-05-26 09:52:47 +0800

    test

--- MyFarm/app/src/main/java/com/example/zhangnan/myfarm/ControlActivity.java
+++ MyFarm/app/src/main/java/com/example/zhangnan/myfarm/ControlActivity.java
@@ -83,6 +83,7 @@ public class ControlActivity extends AppCompatActivity {
         public SoundHodler onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater inflater= LayoutInflater.from(ControlActivity.this);
             View view=inflater.inflate(R.layout.control_list_item,parent,false);
+            //123
             return new SoundHodler(view);
         }
 
